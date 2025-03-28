0ca592f66c1dbd4cff8f11de85f530408769d54d
oramirezperera <ramirez.orlando17@gmail.com>
2025-03-28 16:56:00 -0400

    changes to the load.py script, nowwe don't use replace, instead we use append, because we changed the schema directly in the database management

--- load.py
+++ load.py
@@ -6,7 +6,7 @@ def main():
     df = pd.read_csv('./data/netflix_titles.csv')
     engine = sal.create_engine(connection.get_connection())
     conn = engine.connect()
-    df.to_sql('netflix_raw', con=conn, index=False, if_exists='replace')
+    df.to_sql('netflix_raw', con=conn, index=False, if_exists='append')
 
 if __name__ == '__main__':
     main()
